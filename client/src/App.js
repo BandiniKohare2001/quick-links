4416032f1764cc26197b19de41b69ee6219c19ff
Bandini2001 <bandinikohare30@gmail.com>
2023-11-23 16:33:06 +0530

    create build folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,81 +1,240 @@
-import React, { useState } from 'react';
-import './App.css';
-import copyicon from './copyicon.png';
-import axios from 'axios';
+// import React, { useState } from 'react';
+// import './App.css';
+// import copyicon from './copyicon.png';
+// import axios from 'axios';
+
+// function App() {
+//   const [url, setUrl] = useState("");
+//   const [slug, setSlug] = useState("");
+//   const [shortUrl, setShortUrl] = useState("");
+
+
+//   // const generateLink = async () => {
+//   //   try {
+//   //     const response = await axios.post('/link', { 
+//   //       url, 
+//   //       slug 
+//   //     });
+  
+//   //     setShortUrl(response?.data?.data?.shortUrl);
+//   //   } catch (error) {
+//   //     console.error("Error generating link:", error);
+//   //   }
+//   // }
+//   const  generateLink = async () => {
+//     const response = await axios.post('/link', {
+//       url,
+//       slug
+//     })
+
+//     setShortUrl(response?.data?.data?.shortUrl)
+//   }
+
+//   const copyShorturl = () => {
+//     navigator.clipboard.writeText(shortUrl);
+//     alert("URL copied");
+//   }
+
+
+//   return (
+//     <>
+//       <h1 className='text-center main-heading'><span className='colour-dark'>QUicK</span>LiNkS</h1>
+//       <div className='app-container'>
+//         <div className='link-generation-card'>
+//           <h2 >Link Generation</h2>
+//           {/* <input
+//             type='text'
+//             placeholder='URL'
+//             className='url-input'
+//             value={url}
+//             onChange={(e) => {
+//               setUrl(e.target.value)
+//             }}
+//           />
+//           <input
+//             type='text'
+//             placeholder='Slug (optional)'
+//             className='url-input'
+//             value={slug}
+//             onChange={(e) => {
+//               setSlug(e.target.value)
+//             }}
+//           />
+//           <div className='shorturl-container'>
+//             <input
+//               type='text'
+//               placeholder='Short URL'
+//               className='short-url-input'
+//               value={shortUrl}
+//               disabled
+//             /> */}
+//             <input
+//             type='text'
+//             placeholder='URL'
+//             className='user-input'
+//             value={url}
+//             onChange={(e) => {
+//               setUrl(e.target.value)
+//             }} />
+
+//           <input
+//             type='text'
+//             placeholder='Slug (optional)'
+//             className='user-input'
+//             value={slug}
+//             onChange={(e) => {
+//               setSlug(e.target.value)
+//             }} />
+
+//           <div className='short-url-container'>
+//             <input
+//               type='text'
+//               placeholder='Short URL'
+//               className='input-short-url'
+//               value={shortUrl}
+//               disabled />
+//             <img src={copyicon}
+//                   alt="copy"
+//                   className="copy-icon"
+//                   onClick={copyShorturl}
+//                   />
+         
+//             {/* <img src={copyicon} onClick={copyShorturl} className='coyp-icon' alt='Copy Icon' /> */}
+//           </div>
+//           <button className='btn url-input' onClick={generateLink}>Generate Short URL</button>
+//         </div>
+//         <div>
+
+//         <h2>All Links</h2>
+      
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import copyImg from './copyicon.png'
 
 function App() {
-  const [url, setUrl] = useState("");
-  const [slug, setSlug] = useState("");
-  const [shortUrl, setShortUrl] = useState("");
+  const [url, setUrl] = useState('')
+  const [slug, setSlug] = useState('')
+  const [shortUrl, setShortUrl] = useState('')
+  const [links, setLinks] = useState([])
 
-
-  // const generateLink = async () => {
-  //   try {
-  //     const response = await axios.post('/link', { 
-  //       url, 
-  //       slug 
-  //     });
-  
-  //     setShortUrl(response?.data?.data?.shortUrl);
-  //   } catch (error) {
-  //     console.error("Error generating link:", error);
-  //   }
-  // }
-  const  generateLink = async () => {
+  const generateLink = async () => {
     const response = await axios.post('/link', {
       url,
       slug
     })
-
     setShortUrl(response?.data?.data?.shortUrl)
   }
 
-  const copyShorturl = () => {
+  const copyShortUrl = () => {
     navigator.clipboard.writeText(shortUrl);
-    alert("URL copied");
+    alert('Copied to clipboard!')
   }
 
+  const loadLinks = async () => {
+    const response = await axios.get('/api/links');
+    setLinks(response?.data?.data)
+  }
+
+  useEffect(() => {
+    loadLinks();
+  }, [])
 
   return (
-    <>
-      <h1 className='text-center main-heading'><span className='colour-dark'>QUicK</span>LiNkS</h1>
+    <div>
+       <h1 className='text-center main-heading'><span className='colour-dark'>QUicK</span>LiNkS</h1>
+
       <div className='app-container'>
-        <div className='link-generation-card'>
-          <h2 >Link Generation</h2>
-          <input
-            type='text'
+      <div className='link-generation-card'>
+        <h2 >Link Generation</h2>
+          <input type='text'
             placeholder='URL'
             className='url-input'
             value={url}
-            onChange={(e) => setUrl(e.target.value)}
+            onChange={(e) => {
+              setUrl(e.target.value)
+            }}
           />
+
           <input
             type='text'
-            placeholder='Slug (optional)'
+            placeholder='Slug(optional)'
             className='url-input'
             value={slug}
-            onChange={(e) => setSlug(e.target.value)}
+            onChange={(e) => {
+              setSlug(e.target.value)
+            }}
           />
-          <div className='shorturl-container'>
+
+         <div className='short-url-container'>
             <input
               type='text'
               placeholder='Short URL'
               className='short-url-input'
               value={shortUrl}
-              disabled
-            />
-            <img src={copyicon} onClick={copyShorturl} className='coyp-icon' alt='Copy Icon' />
-          </div>
-          <button className='btn url-input' onClick={generateLink}>Generate Short URL</button>
-        </div>
-        <div>
+              disabled />
 
-        <h2>All Links</h2>
-      
+            <img
+              src={copyImg}
+              alt='copy'
+              className='coyp-icon'
+              onClick={copyShortUrl}
+            />
+
+          </div>
+
+          <button
+            type='button'
+            className='btn url-input'
+            onClick={generateLink}>
+            Generate Quick Link
+          </button>
         </div>
+
+        <div className='all-links-container'>
+     
+          {
+            links?.map((linksObj, index)=>{
+              const {url, slug, clicks} = linksObj;
+
+              return(
+               
+                <div className='link-card'>
+                  <p>
+                    <span className='link-text'>
+                      URL : </span>
+                    <span className='text-url'> {url}</span>
+                  </p>
+                  <p>
+                    <span className='link-text'>
+                      Slug : </span>   
+
+                       <span className='text-url'> 
+                       {process.env.REACT_APP_BASE_URL}/{slug}
+                    
+                    </span>
+                   
+                  </p>
+                  <p>
+                    <span className='link-text'>Clicks : </span>
+                    <span className='text-url'>{clicks}</span></p>
+                  </div>
+              )
+            })
+          }
+        </div>
+       
       </div>
-    </>
-  );
+    </div>
+    
+  )
 }
 
-export default App;
+export default App
--- server/index.js
+++ server/index.js
@@ -1,93 +1,92 @@
+
 import express from "express";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-import Link from "./models/link.js";
+import path from 'path';
+
+import Link from './models/Link.js';
 
 const app = express();
 app.use(express.json());
 
+const __dirname = path.resolve();
+
 const connectDB = async ()=>{
-    const conn = await mongoose.connect(process.env.MONGODB_URI)
-
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     if(conn){
-        console.log("MongoDB Connected");
+        console.log(`MongoDB is connected 💖`);
     }
-}
-
+};
 connectDB();
 
-// app.post("/link",async (req, res)=>{
-//     const {url, slug} = req.body;
-// const randonSlug = Math.random().toString(36).substring(2, 7);
-//     const link = new Link({
-//         url: url,
-//         slug: slug || randonSlug
-//     })
-//     try{
-//         const savedLink = await link.save();
-//         return res.json({
-//             success: true,
-//             data: {
-//                 shortLink: `${process.env.BASE_URL}/${savedLink.slug}`
-//             },
-//             message: "Link saved Successfully"
-//         });
-//     }
-//     catch(err){
-//         return res.json({
-//             success: false,
-//             message: err.message
-//         });
-//     }
-// })
-app.post("/link", async (req, res) => {
-  const { url, slug } = req.body;
+app.post('/link',async (req,res)=>{
+    const {url, slug} = req.body;
 
-  const randomSlug = Math.random().toString(36).substring(2, 7);
+    const randomSlug = Math.random().toString(36).substring(2, 7);
+    
+    const linkObj = new Link({
+        url:url,
+        slug:slug || randomSlug,
+    })
+    try{
+        const savedLink = await linkObj.save();
 
-  const link = new Link({
-    url: url,
-    slug: slug || randomSlug,
-  });
+       return res.json({
+            success:true,
+            data:{
+                shortUrl: `${process.env.BASE_URL}/${savedLink.slug}`
+            },
+            message:"successfully created link"
+        })
+    }
+   catch(err){
+    res.json({
+        success:false,
+        message:err.message
+    })
+   }
+   
+})
 
-  try {
-    const savedLink = await link.save();
-
-    return res.json({
-      success: true,
-      data: {
-        shortUrl: `${process.env.BASE_URL}/${savedLink.slug}`,
-      },
-      message: "Link saved successfully",
-    });
-  } catch (err) {
-    return res.json({
-      success: false,
-      message: err.message,
-    });
-  }
-});
-
-
-app.get("/:slug", async (req, res)=>{
+app.get('/:slug',async (req,res)=>{
     const {slug} = req.params;
-    const link = await Link.findOne({slug: slug});
 
-    await Link.updateOne({slug: slug}, {$set: {clicks: link.clicks + 1}})
+    const link = await Link.findOne({slug:slug});
 
     if(!link){
         return res.json({
-            success: false,
-            message: "Link not Found"
+            success:false,
+            message:"link not found"
         })
-    }
+    } 
+    
+    await Link.updateOne({slug: slug}, {$set: {clicks: link.clicks +1
+    }})
+    
     res.redirect(link.url);
 })
 
+app.get('/api/links',async(req,res)=>{
+    const links = await Link.find();
+
+    res.json({
+        success:true,
+        data:links,
+        message:'Links fetch successfully'
+    })
+})
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+    });
+  }
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-});
+app.listen(PORT, ()=>{
+    console.log(`server is running on ${PORT}`);
+})
